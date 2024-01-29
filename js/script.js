569aed83472707f9354f27f9c1431d82979d8408
Matheus Mendes <matheus.mendes@somosicev.com>
2024-01-29 00:08:15 -0300

    Primeiro comiit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector('.mario')
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = window.getComputedStyle(mario).bottom.replace
+    ('px', "")
+   
+    if (pipePosition <_ 120 && pipePosition > 0 && marioPosition < 80) {
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = ${pipePosition}px';
+
+        mar
+    }
+
+
+
+
+
+
+}, 10);
+
+document.addEventListener('keydown', jump)
